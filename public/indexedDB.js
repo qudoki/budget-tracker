fa0d9a32de02ccf49c6c553eb2d4c8e144294614
Qian Dong <qdong327@gmail.com>
2020-12-11 07:27:16 -0500

    fixed database file

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,21 +1,70 @@
-function saveRecord(data) {
-	console.log(data);
+const indexedDB = 
+window.indexedDB ||
+window.mozIndexedDB ||
+window.webkitIndexedDB ||
+window.msIndexedDB ||
+window.simIndexedDB;
+
+let db;
+const request = indexedDB.open("budget", 1);
 
 // above register service worker
 // check for service worker
 
 // service worker lifecycle
 
-    // install -> create bulk (pending) collection
+//schema
+request.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createObjectStore("pending", { autoIncrement: true});
+};
 
-    // activate -> 
+//if online check DB
+// request.onsuccess = ({ target }) => {
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
 
-
-    // saveRecord (if it's going to fail) -> save to indexedDB
-
-    // listen for when we are back online and send records
-
-
-
-
+//deal with it
+request.onerror = function(event) {
+    console.log("Error!" + " " + event.target.errorCode)
 }
+
+//save record to db (called if offline - request fail)
+function saveRecord(data) {  
+    const transaction = db.transaction(["pending"], "readWrite"); 
+    const store = transaction.objectStore("pending");
+    store.add(data);
+}
+
+//back online, send to mongo and clear pending (indexedDB)
+function checkDatabase() {
+    //below is failing, !!! check here
+    const transaction = db.transaction(["pending"], "readWrite"); 
+    const store = transaction.objectStore("pending");
+    const getAllTrans = store.getAllTrans();
+
+    getAllTrans.onsuccess = function () {
+        if (getAllTrans.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAllTrans.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+    .then(response => response.json())
+    .then(() => {
+        const transaction = db.transaction(["pending"], "readWrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+    });
+        }
+    };
+}
+//listen for app coming back online
+window.addEventListener("online", checkDatabase);

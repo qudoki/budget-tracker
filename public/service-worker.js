ccc0783292c371650bd33974633aa2933d73f1fa
Qian Dong <qdong327@gmail.com>
2020-12-12 17:46:33 -0500

    finished service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,3 +9,66 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/indexedDB.js"
 ]
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (e) {
+    if (e.request.url.includes("/api/")) {
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(e.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(e.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(e.request);
+                    });
+            }).catch(err => {
+                console.log(err)
+            })
+        );
+
+        return;
+    }
+    e.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(e.request).then(response => {
+                return response || fetch(e.request);
+            });
+        })
+    );
+});

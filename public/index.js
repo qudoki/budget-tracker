91a81de9596ff61ea0e7c8e601c522d6aa44fc4f
Qian Dong <qdong327@gmail.com>
2020-12-12 17:44:27 -0500

    mod service worker file

--- public/index.js
+++ public/index.js
@@ -1,13 +1,3 @@
-// register service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("service-worker.js")
-    .then((reg) => {
-    console.log("We found your service worker file!", reg);
-    });
-  });
-}
-
 let transactions = [];
 let myChart;
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,11 @@
 console.log("Hi from your service-worker.js file!");
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/service-worker.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/indexedDB.js"
+]
